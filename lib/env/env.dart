f811396c830faf2104d54d9b701cde5f369f056f
yusuke-ogiso-adglobe-co-jp <yusuke.ogiso@mail.adglobe.co.jp>
2024-05-27 18:41:13 +0900

    envの設定

---
+++ lib/env/env.dart
@@ -0,0 +1,8 @@
+import 'package:envied/envied.dart';
+part 'env.g.dart';
+
+@Envied(path: '.env')
+abstract class Env {
+  @EnviedField(varName: 'KEY', obfuscate: true)
+  static String key = _Env.key;
+}
