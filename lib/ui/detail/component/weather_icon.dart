ba5fd0996a797784e890e0c684f754ff5c8f20ae
yusuke-ogiso-adglobe-co-jp <yusuke.ogiso@mail.adglobe.co.jp>
2024-06-04 16:07:39 +0900

    WeathreIconとしてConsumerWidgetに切り出し

---
+++ lib/ui/detail/component/weather_icon.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:weather_app_flutter_mvvm/ui/detail/view_model/detail_view_model.dart';
+
+class WeathreIcon extends ConsumerWidget {
+  const WeathreIcon(this.iconName, {super.key});
+
+  final String iconName;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final image = ref
+        .watch(detailViewModelProvider.notifier)
+        .fetchIconImage(iconName: iconName);
+    return FutureBuilder(
+      future: image,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.hasData) {
+          return Image.memory(
+            snapshot.data,
+            width: 50,
+            height: 50,
+          );
+        } else {
+          return const Text('No data');
+        }
+      },
+    );
+  }
+}
