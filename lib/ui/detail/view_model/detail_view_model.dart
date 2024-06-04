ff6411c8394f044735e6a8f3d621862b56c56610
yusuke-ogiso-adglobe-co-jp <yusuke.ogiso@mail.adglobe.co.jp>
2024-06-04 18:23:47 +0900

    detail_error_stateを作成しViewModelと連携

--- lib/ui/detail/view_model/detail_view_model.dart
+++ lib/ui/detail/view_model/detail_view_model.dart
@@ -2,6 +2,7 @@ import 'dart:typed_data';
 
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:weather_app_flutter_mvvm/repository/weather_repository_provider.dart';
+import 'package:weather_app_flutter_mvvm/ui/detail/ui_state/detail_error_state.dart';
 import 'package:weather_app_flutter_mvvm/ui/detail/ui_state/detail_ui_state.dart';
 
 part 'detail_view_model.g.dart';
@@ -17,16 +18,21 @@ class DetailViewModel extends _$DetailViewModel {
     String city,
   ) async {
     state = const AsyncLoading();
-    state = await AsyncValue.guard(
-      () async {
-        final response =
-            await ref.read(weatherRepositoryProvider).fetchWeatherByCity(
-                  city: city,
-                );
-        final uiState = DetailUiState.fromWeatherResponse(response);
-        return uiState;
-      },
-    );
+    try {
+      final response =
+          await ref.read(weatherRepositoryProvider).fetchWeatherByCity(
+                city: city,
+              );
+      final uiState = DetailUiState.fromWeatherResponse(response);
+      state = AsyncValue.data(uiState);
+    } catch (error, stackTrace) {
+      final errorMessage = error.toString().replaceFirst(
+            'Exception: ',
+            '',
+          );
+      ref.read(detailErrorStateProvider.notifier).state = errorMessage;
+      state = AsyncError(error, stackTrace);
+    }
   }
 
   Future<void> fetchWeatherByLocation({
@@ -34,17 +40,22 @@ class DetailViewModel extends _$DetailViewModel {
     required double lon,
   }) async {
     state = const AsyncLoading();
-    state = await AsyncValue.guard(
-      () async {
-        final response =
-            await ref.read(weatherRepositoryProvider).fetchWeatherByLocation(
-                  lat: lat,
-                  lon: lon,
-                );
-        final uiState = DetailUiState.fromWeatherResponse(response);
-        return uiState;
-      },
-    );
+    try {
+      final response =
+          await ref.read(weatherRepositoryProvider).fetchWeatherByLocation(
+                lat: lat,
+                lon: lon,
+              );
+      final uiState = DetailUiState.fromWeatherResponse(response);
+      state = AsyncValue.data(uiState);
+    } catch (error, stackTrace) {
+      final errorMessage = error.toString().replaceFirst(
+            'Exception: ',
+            '',
+          );
+      ref.read(detailErrorStateProvider.notifier).state = errorMessage;
+      state = AsyncError(error, stackTrace);
+    }
   }
 
   Future<Uint8List> fetchIconImage({
