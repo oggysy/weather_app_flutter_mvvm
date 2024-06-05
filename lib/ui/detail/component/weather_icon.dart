e08c96bf1f2bb3ba9a17f1498898445cddd223d9
yusuke-ogiso-adglobe-co-jp <yusuke.ogiso@mail.adglobe.co.jp>
2024-06-05 13:37:53 +0900

    icon取得メソッドをviewModelから分離

--- lib/ui/detail/component/weather_icon.dart
+++ lib/ui/detail/component/weather_icon.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:weather_app_flutter_mvvm/ui/detail/view_model/detail_view_model.dart';
+import 'package:weather_app_flutter_mvvm/ui/detail/component/provider/fetch_icon_image_provider.dart';
 
 class WeathreIcon extends ConsumerWidget {
   const WeathreIcon(this.iconName, {super.key});
@@ -9,26 +9,20 @@ class WeathreIcon extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final image = ref
-        .watch(detailViewModelProvider.notifier)
-        .fetchIconImage(iconName: iconName);
-    return FutureBuilder(
-      future: image,
-      builder: (BuildContext context, AsyncSnapshot snapshot) {
-        if (snapshot.hasData) {
-          return Image.memory(
-            snapshot.data,
-            width: 50,
-            height: 50,
-          );
-        } else if (snapshot.connectionState == ConnectionState.waiting) {
-          return const SizedBox(
-            width: 50,
-            height: 50,
-          );
-        } else {
-          return const Text('No data');
-        }
+    final image = ref.watch(fetchIconImageProvider(iconName));
+    return image.maybeWhen(
+      orElse: () {
+        return const SizedBox(
+          width: 50,
+          height: 50,
+        );
+      },
+      data: (data) {
+        return Image.memory(
+          data,
+          width: 50,
+          height: 50,
+        );
       },
     );
   }
