0d37b7b1d8a0b26a003558d9b40d624bdd65d809
yusuke-ogiso-adglobe-co-jp <yusuke.ogiso@mail.adglobe.co.jp>
2024-06-05 13:51:00 +0900

    各Viewに反映

--- lib/ui/prefecture/view/prefecture_page.dart
+++ lib/ui/prefecture/view/prefecture_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app_flutter_mvvm/ui/detail/view/detail_for_city_page.dart';
 import 'package:weather_app_flutter_mvvm/ui/detail/view/detail_page.dart';
 
 class PrefecturePage extends StatelessWidget {
@@ -72,7 +73,7 @@ class PrefecturePage extends StatelessWidget {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => DetailPage(prefectures[index]),
+                  builder: (context) => DetailForCityPage(prefectures[index]),
                   fullscreenDialog: true,
                 ),
               );
