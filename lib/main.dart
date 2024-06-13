9107ccd05248ff95a2b0fb97ffccc8f9960045f4
yusuke-ogiso-adglobe-co-jp <yusuke.ogiso@mail.adglobe.co.jp>
2024-06-13 10:32:45 +0900

    DateTimeにTZDateTimeへの変換メソッドを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weather_app_flutter_mvvm/ui/home/view/home_page.dart';
+import 'package:timezone/data/latest.dart' as tz;
 
 void main() {
+  tz.initializeTimeZones();
   runApp(const ProviderScope(child: MainApp()));
 }
 
